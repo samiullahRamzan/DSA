5cc91d5e3fa9ba2779b7e6c46e9cabe37cd215dc
SamiUllah-dev336 <samidev336@gmail.com>
2025-06-01 11:33:47 +0500

    LinkedlistClass methods=> addFirst - addLast - printlist

---
+++ Linked_List/src/LinkedList.java
@@ -0,0 +1,58 @@
+public class LinkedList {
+    Node head;
+
+    public void addFirst(String data){
+        Node newNode=new Node(data);
+
+        if(head==null){
+            head=newNode;
+            return;
+        }
+
+        newNode.next=head;
+        head=newNode;
+    }
+
+    public void addLast(String data){
+        Node newNode=new Node(data);
+
+        if(head==null){
+            head=newNode;
+            return;
+        }
+
+        Node currNode=head;
+        while(currNode.next!=null){
+            currNode=currNode.next;
+        }
+
+        currNode.next=newNode;
+    }
+
+    public void printList(){
+        if(head==null){
+            System.out.println("List is empty!");
+            return;
+        }
+        Node currNode=head;
+
+        while(currNode!=null){
+            System.out.print(currNode.data+"-->");
+            currNode=currNode.next;
+        }
+
+        System.out.print("Null");
+    }
+
+    public String LinkedListCenter(){
+        Node s_pointer=head;
+        Node f_pointer=head;
+
+        while(f_pointer.next!=null){
+            s_pointer=s_pointer.next;
+            f_pointer=f_pointer.next.next;
+        }
+
+        return s_pointer.data;
+    }
+}
---
+++ Linked_List/src/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+       LinkedList LL=new LinkedList();
+
+       LL.addLast("Now");
+       LL.addLast("i");
+       LL.addLast("am");
+       LL.addLast("at");
+       LL.addLast("Lahore");
+
+       LL.printList();
+
+    }
+}
