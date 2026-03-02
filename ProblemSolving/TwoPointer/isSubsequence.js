cf4d80a3fec7902e7fefb42091b9a42a856fd1ee
samiullah-dev <samidev336@gmail.com>
2026-03-02 19:39:27 +0500

    isSubsequence

---
+++ ProblemSolving/TwoPointer/isSubsequence.js
@@ -0,0 +1,18 @@
+
+
+var isSubsequence = function(s, t) {
+
+    let p1=0,p2=0;
+
+    while(p1 < s.length && p2 <t.length){
+
+        if(s[p1]===t[p2]){
+            p1++;
+        }
+        p2++;
+   
+    }
+
+    return p1===s.length
+ 
+};
