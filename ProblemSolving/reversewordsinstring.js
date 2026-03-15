12dc6fa0ab12da25c0e88120fc1c16a2d7885e5a
samiullah-dev <samidev336@gmail.com>
2026-03-15 09:35:37 +0500

    updated

--- ProblemSolving/reversewordsinstring.js
+++ ProblemSolving/reversewordsinstring.js
@@ -1,10 +1,67 @@
 const reverseString = (str) => {
-  let reversed = "";
+    let reversed = "";
 
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
 
-  return reversed;
+    return reversed;
 };
 
+const isLetterOrNumber = (char) => {
+    //   const code = char.charCodeAt(0);
+    //   return (
+    //     (code >= 65 && code <= 90) || // A–Z
+    //     (code >= 97 && code <= 122) || // a–z
+    //     (code >= 48 && code <= 57) // 0–9
+    //   );
+
+    // another way...
+
+    return (
+        (char >= 'A' && char <= 'Z') ||
+        (char >= 'a' && char <= 'z') ||
+        (char >= '0' && char <= '9')
+    );
+};
+
+
+var reverseWords = function(s) {
+
+    s = reverseString(s);   
+
+    let startIndex=0;
+    let endIndex=0;
+    let index=0;
+
+    let reverseWordsinstr='';
+
+    for(let i=0;i<=s.length;i++){
+
+        if(isLetterOrNumber(s[i]) ){
+
+            if(index==0){
+                startIndex=i;
+            }
+
+            index++;
+        }else{
+
+            if(index>0){
+                endIndex=i-1;
+
+                for(let j=endIndex; j>=startIndex;j--){
+                    reverseWordsinstr+=s[j];
+                }
+
+                if(endIndex != s.length-1){
+                    reverseWordsinstr+=' ';
+                }
+
+                index=0;
+            }
+        }
+    }
+
+    return reverseWordsinstr.trim();
+};
