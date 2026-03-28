789f0c91548260c944277ddb006896be0a6fe35b
samiullah-dev <samidev336@gmail.com>
2026-03-28 23:23:22 +0500

    updated

--- ProblemSolving/productElementsExceptSelf.js
+++ ProblemSolving/productElementsExceptSelf.js
@@ -13,3 +13,21 @@ for(let i=0;i<arr.length;i++){
 
 console.log('here is arr',arr);
 console.log('here is new array...',newArr);
+
+//  best case O(n) time complexity and O(1) space complexity
+var productExceptSelf = function(nums) {
+    let n = nums.length;
+    let ans = new Array(n).fill(1);
+
+    for (let i = 1; i < n; i++) {
+        ans[i] = ans[i - 1] * nums[i - 1];
+    }
+
+    let suffix = 1;
+    for (let i = n - 2; i >= 0; i--) {
+        suffix *= nums[i + 1];
+        ans[i] *= suffix;
+    }
+
+    return ans;
+};
